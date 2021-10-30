ed9514cb9be2acfc8fc04c828fe036bb0fe0a0f7
trmyo <64032492+trmyo@users.noreply.github.com>
2021-10-30 14:37:01 +0400

    gitignore for log_dir

--- src/training.py
+++ src/training.py
@@ -20,7 +20,7 @@ def training(config_path):
     EPOCHS = config["params"]["epochs"]
     VALIDATION_SET = (X_valid, y_valid)
 
-    CALLBACK_LISt=get_callbacks(config,X_train)
+    CALLBACK_LIST=get_callbacks(config,X_train)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION_SET,callbacks=CALLBACK_LIST)
     artifacts_dir = config["artifacts"]["artifacts_dir"]
